caf2897d8d4ca8dcfbe24c918828b9ecd42709b3
ntritin62 <21522677@gm.uit.edu.vn>
2023-11-12 01:34:29 +0700

    Maze Project

--- FirstProgram/src/FirstProgram.java
+++ FirstProgram/src/FirstProgram.java
@@ -1,35 +1,8 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-
-
-
 public class FirstProgram {
 
 	public static void main(String[] args) {	
 		
-		
-		LinkedList<String> names = new LinkedList<String>(); 
-		names.push("caleb"); 
-		names.push("Sue"); 
-		names.push("sally");
-		
-		ListIterator<String> it = names.listIterator();
-
-		it.next();
-		it.next();
-		it.add("Susan");
-		
-		
-		
-		for(String s:names) {
-			System.out.println(s);
-		}
+	
 		
 	}
 
