156d2779d6f39fa099ed9f69bad5a05a312490e5
ntritin62 <21522677@gm.uit.edu.vn>
2023-11-21 20:02:34 +0700

    section-2-7

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Tin";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println('Bob'+ ' ' + '');
+	}
+}
