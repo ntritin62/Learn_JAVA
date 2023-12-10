924353e0513b7ca03440a7cb9a5f4c1525a0686d
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-11 00:45:59 +0700

    section-2-32

---
+++ Tutorial31/src/App.java
@@ -0,0 +1,22 @@
+class Plant {
+	public static final int ID = 7;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
