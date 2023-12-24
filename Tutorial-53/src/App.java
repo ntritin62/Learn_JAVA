47a9f44db66035ad290ceeceec8f2a9b5f4b4af7
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-24 16:37:58 +0700

    section-2-53

---
+++ Tutorial-53/src/App.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class App {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+		numbers.add(10);
+		numbers.add(100);
+		numbers.add(40);
+
+		System.out.println(numbers.get(0));
+
+		System.out.println("\nInteration #1: ");
+		for (int i = 0; i < numbers.size(); i++) {
+			System.out.println(numbers.get(i));
+		}
+
+		System.out.println("\nInteration #2: ");
+		for (Integer value : numbers) {
+			System.out.println(value);
+		}
+
+		numbers.remove(numbers.size() - 1);
+
+	}
+
+}
