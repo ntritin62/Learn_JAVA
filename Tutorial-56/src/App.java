a4d8f4d7b3182fe759722a94abfa48bf8ea98578
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-25 03:49:59 +0700

    section-3-56

---
+++ Tutorial-56/src/App.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class App {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+		LinkedHashMap<Integer, String> linkedhashMap = new LinkedHashMap<Integer, String>();
+		TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>();
+	}
+
+}
